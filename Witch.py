7d6fc13e8194747b9d45fd6128e140b936843a46
Michael Gaynor <jmichaelgaynor@gmail.com>
2017-04-26 10:12:21 -0400

    placing image in class

--- Witch.py
+++ Witch.py
@@ -4,6 +4,45 @@ class Witch(object):
     self.power = 60
     self.name = "unholy Witch"
     self.xp_value = 150
+
+  def print_image(self):
+    print("MMMMMMMMMMMMmoyy++sysyydmNNmdmNNNmmddhhhhyyyo++++/")
+    print("MMMMMMMMMMMdy:-`.``````` `.`.:+hmmmdddhhhhyooo++//")
+    print("MMMMMMMMMMy`````.`....```````  `-dmmdddhhsooo+++//")
+    print("MMMMMMMMMs````-+sydmdhddhys+/.`` /dddddhsoso+++++/")
+    print("MMMMMMMM+ ``-yNMMMMMMMNNNNmhyo:.` -hdhssysoo++++//")
+    print("MMMMMMMd``./mMMMMMMMMNNNmmdysoo+-` `oyyysooo+++///")
+    print("MMMMMMMs..:NNMMMMMMMNNNmmdhsso++/.  .sysssso++////")
+    print("MMMMMMM/--yNNNMMMMMMdyNNmmdhyoo/:-`  :syssoo+++///")
+    print("MMMMMMh--.do-.:+ymNd:-shy+-`   ``.   `+soo+++++///")
+    print("MMMMMM+-``....``  ````           `    .+oooo+++///")
+    print("MMMMMN:. `` Y O U R ``:. E N D     `   .:///++////")
+    print("MMMMMN-. `-:-.`../+-+-  ...``          .-/++/:////")
+    print("MMMMMh.. `:/++ydNd:+o+:`.ss+:.`        `:./+++/://")
+    print("MMNMMs-.`./syMMMNdmNNmy/.:mmdy:.`       ---/+++///")
+    print("MMMMmo-``/smNMMMMNsos:` `.sdhs/.``      --/:/+///:")
+    print("MMMMd+.` `/mmNNNMMMMNmhsssys+:.``    `-..///:////:")
+    print("MMMMm-.` `.d A P P R O A C H E S````  .---/+/:///:")
+    print("MMMNm-` ``.+mmmdhyshoos+:.`.````     `----//-/::/:")
+    print("NNNmd-` ` ` /hhhhhyo/-:-..`````      `-...-:::://:")
+    print("NNNmh/:  `   .sshdmmNmdhs:.`        `.-. ``-/::///")
+    print("mmmys//  .`` `ys::+oso+/.`          .``.`  .//:///")
+    print("mmdsoo/ ``..` /dsh+.``              `````.:///////")
+    print("dds///:  `./. `ssmdhs/.          ```::``::::/+////")
+    print("+:++/--`  `//  ./hddhys/-` .--:::..:o-`o+-:-`///::")
+    print("mdo..--.```.:.  `+hyyso++/.  `-:-.-:o.-oo/--::::--")
+    print("dho` .-.    `    .oyooooso..://++:-::/:///-://:---")
+    print("s/.   ``         ./ysosyho/:/oss:..-/o:+//:-----..")
+    print("-`               //osooyhy++-`` `::-so:+s//-.`````")
+    print("``               -//ossyyoy/```-/+.o//../---`     ")
+    print(".                `/:oyyyss+.--o+/-.o:-` ` ` ````  ")
+    print("``               `:/syyyso:` `::--`.-/      ````  ")
+    print(" `                -+oyyys+. --..-`  :/    ` ````  ")
+    print("                  :/+ssss: ``````   `        `    ")
+    print(" `                -s//+o+.  ``                    ")
+    print("       `          -++-++/`  `:``                  ")
+    print("                  .//:/+:  `//-`                  ")
+    print("                  `:/-:/-` -+//.                  ")
     
   def take_damage(self, damage):
     self.health -= damage
